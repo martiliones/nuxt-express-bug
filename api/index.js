2b52b9b1078fdef47cb8278ba4c96f6ca6024079
martiliones <martiliones@yandex.com>
2021-11-04 14:00:12 +0600

    Delete middlewares

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get('/projects', (req, res) => {
   res.json([
     {
